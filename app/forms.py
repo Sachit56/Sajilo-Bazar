9a8e7ea8295b8b910c56b39b131c8945950493c7
Sachit Khadka <sachitkhadka73@gmail.com>
2024-01-06 15:00:43 +0545

    forms

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-class Registration()
+class Registration(UserCreationForm):
+    username=forms.CharField(label='Username',max_length=100,widget=forms.TextInput({'class':'form-control'}))
+    email=forms.EmailField(label='Email',widget=forms.EmailInput(attrs={'class':'form-control'}))
+    password1=forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2=forms.CharField(label='Repeat Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+    class Meta:
+        model=User
+
+        fields='__all__'
